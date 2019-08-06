e85b960613b65e3f0582fd7d07b93704a54fc008
The-Sun <hirovjp@ubuntu.ubuntu-domain>
2019-08-06 09:32:30 +0700

    Add controller product resource

--- app/Http/Controllers/ProductController.php
+++ app/Http/Controllers/ProductController.php
@@ -38,7 +38,7 @@ class ProductController extends Controller
      */
     public function store(Request $request)
     {
-        $product = new Product();
+        $product = Product::create($request->all());
         return ProductResource::collection($product);
     }
 
@@ -73,7 +73,7 @@ class ProductController extends Controller
      */
     public function update(Request $request, Product $product)
     {
-        //
+        return $product->update($request->all());
     }
 
     /**
